6afab65c75324c187baeee4b045bc0957a9965fb
avocadojoe <vaughnmarchione@icloud.com>
2016-02-07 11:58:45 -0500

    added mtl renderer

--- net/steel3d/graphics/Graphics.java
+++ net/steel3d/graphics/Graphics.java
@@ -1,8 +1,5 @@
 package net.steel3d.graphics;
 
-import java.nio.FloatBuffer;
-
-import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.util.vector.Vector3f;
 import org.newdawn.slick.Color;
@@ -39,52 +36,9 @@ public class Graphics {
 		GL11.glTranslatef(translation.x, translation.y, translation.z);
 	}
 	
-	/**
-	 * draws a light at specified position
-	 * @param position
-	 */
-	public void light(Vector3f position){
-		float[] pos = new float[]{position.x,position.y,position.z,0.0F};
-		FloatBuffer b = BufferUtils.createFloatBuffer(4);
-		b.put(pos);
 
-
-        GL11.glLight(GL11.GL_LIGHT1, GL11.GL_POSITION,(FloatBuffer)b.flip());  
-	}
 	
 	
-	/**
-	 * draws a colored model at specified coordinates
-	 * @param x
-	 * @param y
-	 * @param z
-	 * @param model
-	 */
-	public void drawModel(float x,float y, float z,Model model){
-		GL11.glPushMatrix();
-		GL11.glTranslatef(x, y, z);
-		GL11.glBegin(GL11.GL_TRIANGLES);
-		for(Face face:model.faces){
-			
-			Vector3f n1 = model.vectorNormals.get((int)face.normal.x -1);
-			GL11.glNormal3f(n1.x, n1.y, n1.z);
-			Vector3f v1 = model.vectors.get((int)face.vertex.x -1);
-			GL11.glVertex3f(v1.x, v1.y, v1.z);
-			
-			Vector3f n2 = model.vectorNormals.get((int)face.normal.y -1);
-			GL11.glNormal3f(n2.x, n2.y, n2.z);
-			Vector3f v2 = model.vectors.get((int)face.vertex.y -1);
-			GL11.glVertex3f(v2.x, v2.y, v2.z);
-			
-			Vector3f n3 = model.vectorNormals.get((int)face.normal.z -1);
-			GL11.glNormal3f(n3.x, n3.y, n3.z);
-			Vector3f v3 = model.vectors.get((int)face.vertex.z -1);
-			GL11.glVertex3f(v3.x, v3.y, v3.z);
-			
-		}
-		GL11.glEnd();
-		GL11.glPopMatrix();
-	}
 	/**
 	 * draws a textured model at the specified coordinates
 	 * @param x
@@ -93,12 +47,17 @@ public class Graphics {
 	 * @param model
 	 * @param texture
 	 */
-	public void drawModel(float x,float y, float z,Model model,Texture texture){
+	public void drawModel(float x,float y, float z,Mesh model){
 		GL11.glPushMatrix();
 		GL11.glTranslatef(x, y, z);
-		texture.bind();
+	
 		GL11.glBegin(GL11.GL_TRIANGLES);
 		for(Face face:model.faces){
+			if(face.mat!=null){
+				face.mat.tex.bind();
+			}
+			GL11.glTexParameteri( GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST );
+
 			Vector3f n1 = model.vectorNormals.get((int)face.normal.x -1);
 			GL11.glNormal3f(n1.x, n1.y, n1.z);
 			Vector3f v1 = model.vectors.get((int)face.vertex.x -1);
@@ -116,6 +75,13 @@ public class Graphics {
 			Vector3f v3 = model.vectors.get((int)face.vertex.z -1);
 			GL11.glTexCoord2f(1, 1);
 			GL11.glVertex3f(v3.x, v3.y, v3.z);
+/**
+			Vector3f n4 = model.vectorNormals.get((int)face.normal.w -1);
+			GL11.glNormal3f(n4.x, n4.y, n4.z);
+			Vector3f v4 = model.vectors.get((int)face.vertex.w -1);
+			GL11.glTexCoord2f(0, 1);
+			GL11.glVertex3f(v4.x, v4.y, v4.z);
+			**/
 		}
 		GL11.glEnd();
 		GL11.glPopMatrix();
