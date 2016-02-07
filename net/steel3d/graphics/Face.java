2d437e2eeec789c2063d84ed70cbed59f4e0b792
avocadojoe <vaughnmarchione@icloud.com>
2016-02-07 11:59:42 -0500

    Update Face.java

--- net/steel3d/graphics/Face.java
+++ net/steel3d/graphics/Face.java
@@ -1,14 +1,18 @@
 package net.steel3d.graphics;
 
-import org.lwjgl.util.vector.Vector3f;
+import org.lwjgl.util.vector.Vector4f;
+
+import net.steel3d.util.Material;
 
 public class Face {
 
-	public Vector3f vertex = new Vector3f();
-	public Vector3f normal = new Vector3f();
+	public Vector4f vertex = new Vector4f();
+	public Vector4f normal = new Vector4f();
+	public Material mat;
 
-	public Face(Vector3f vertex,Vector3f normal){
+	public Face(Vector4f vertex,Vector4f normal,Material mat){
 		this.vertex = vertex;
 		this.normal = normal;
+		this.mat = mat;
 	}
 }
